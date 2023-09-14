36dc176a1c0c09574816c1618efe30fc977a2e46
Malvina666 <marie.lemire@hotmail.com>
2023-09-14 15:35:54 +0200

    Site des savons

---
+++ webfiles/php/actions/dbConnect.php
@@ -0,0 +1,3 @@
+<?php
+$conn = new PDO('mysql:host=localhost;dbname=monptitvaureen;charset=utf8','root','');
+?>
---
+++ webfiles/php/views/footer.php
@@ -0,0 +1,14 @@
+<footer>
+        <div class="footer">
+            <nav>
+                <p>Mentions légales</p>        
+                <p>Partenaires</p>      
+                <div id="buttons">
+                    <p><a href="../users/connect.php">Connexion</a></p>
+                    <p><a href="../users/inscriptionForm.php">Inscription</a></p>
+                </div>
+            </nav>
+        </div>
+    </footer>
+</body>
+</html>
